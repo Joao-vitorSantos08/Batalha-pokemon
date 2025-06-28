021339c20e0a1babdf71c561998aa7893f7d4408
João vitor <joaovitorsantossouza935@gmail.com>
2025-06-28 18:57:02 -0300

    refactor: final tweaks and improvements to Pokémon battle logic

--- src/js/script.js
+++ src/js/script.js
@@ -9,58 +9,33 @@ let pokemonExperience2 = document.getElementById("pokemonExperience2")
 let btnBatalhar = document.getElementById("btnBatalhar")
 let vencedor = document.getElementById("vencedor")
 
-let searchPokemon = Math.floor(Math.random() * 850) + 1;  
+btnBatalhar.addEventListener("click" , function(){
+    let random1 = Math.floor(Math.random() * 649) + 1;
+    let random2 = Math.floor(Math.random() * 649) + 1;
 
-    function pokebola(pokemonId){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${random1}`)
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
         pokemonImg.src = data.sprites.versions["generation-v"]["black-white"].animated.front_default
         pokemonName.innerText = data.name
         pokemonExperience.innerText = data.base_experience
-    })
-    }
 
-    function pokebola2(pokemonId){
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${random2}`)
         .then((res) => res.json())
-         .then((data) => {
-            pokemon2.src = data.sprites.versions["generation-v"]["black-white"].animated.front_default
-         pokemonName2.innerText = data.name
-         pokemonExperience2.innerText =  data.base_experience
-         })
-    }
+        .then((data) =>{
+        pokemon2.src = data.sprites.versions["generation-v"]["black-white"].animated.front_default
+        pokemonName2.innerText = data.name
+        pokemonExperience2.innerText = data.base_experience   
 
+        if(Number(pokemonExperience.innerText) > Number(pokemonExperience2.innerText)){
+            vencedor.innerText = ` 🏆${pokemonName.innerText} venceu a batalha!` 
+        }else if (Number(pokemonExperience2.innerText) > Number(pokemonExperience.innerText)){
+            vencedor.innerText = ` 🏆${pokemonName2.innerText} venceu a batalha!`
+        }else{
+            vencedor.innerText = "Empate"
+        }
 
- btnBatalhar.addEventListener("click", function(){
-    searchPokemon +=1
-    pokebola(searchPokemon)
-    if (Number(pokemonExperience.innerText) > Number(pokemonExperience2.innerText)) {
-        vencedor.innerText = `🏆Vencedor: ${pokemonName.innerText}`
-    }else if(Number(pokemonExperience.innerText) == Number(pokemonExperience2.innerText)){
-        vencedor.innerText = "Empate"
-    }else{
-         vencedor.innerText = `🏆Vencedor: ${pokemonName2.innerText}`
-    }
-    
+        })
+    })
 })
-
-     btnBatalhar.addEventListener("click", function(){
-    searchPokemon +=1
-    pokebola2(searchPokemon)
-     if (Number(pokemonExperience.innerText) < Number(pokemonExperience2.innerText)) {
-        vencedor.innerText = `🏆Vencedor: ${pokemonName2.innerText}`
-    }else if(Number(pokemonExperience.innerText) == Number(pokemonExperience2.innerText)){
-         vencedor.innerText = "Empate"
-    }else{
-        vencedor.innerText = `🏆Vencedor: ${pokemonName.innerText}`
-    }
-    
-})
-
-
-
-pokebola(123)
-pokebola2(204)
-
